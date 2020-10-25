8bac9159e0b41a7476e7f779f54b47f0a75b0f24
Angad Sethi <angadsethi18@gmail.com>
2020-10-25 16:29:00 +0530

    Finalised some code

--- getFrame.js
+++ getFrame.js
@@ -1,42 +1,48 @@
-(function() {
-
+window.onload = function () {
   var logged_in_user;
-
+  
   chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
       logged_in_user = response.info;
   });
 
+  function main_function() {
     var imageCapture;
+    if (document.getElementById('faceTracker') !== null) {
+      document.getElementsByTagName("body")[0].removeChild(document.getElementsByTagName("video")[0]);
+    }
+
     var x = document.createElement('video');
-    x.id = "faceTracker";
+    x.id = "faceTracker";    
     x.autoplay = "autoplay";
-
     document.getElementsByTagName("body")[0].appendChild(x);
-
+    
     navigator.mediaDevices.getUserMedia({video: true})
-    .then(mediaStream => {
-      x.srcObject = mediaStream;
-      const track = mediaStream.getVideoTracks()[0];
-      imageCapture = new ImageCapture(track);
-  })
-  .catch(error => console.log(error));
-
-  var tracker = new tracking.ObjectTracker(['face']);
-  tracking.track('#faceTracker', tracker, { camera: true });
+      .then(mediaStream => {
+        const track = mediaStream.getVideoTracks()[0];
+        document.getElementsByTagName('video')[0].srcObject = mediaStream;
+    }).catch(error => console.log(error));
+  }  
   
-  var xhttp = new XMLHttpRequest();
-  var current_meet_id = window.location.href.split('/');
-
-  tracker.on('track', function(event) {
-    event.data.forEach(function(rect) {
-      console.log(rect);
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log("Cool");
-        }
-      };
-      xhttp.open("GET", BACKEND_URLS.facetracker + '?user=' + logged_in_user.email + '&meet_id=' + current_meet_id[current_meet_id.length - 1], true);
-      xhttp.send();
+  function sendData() {  
+    var tracker = new tracking.ObjectTracker(['face']);
+    tracking.track('#faceTracker', tracker, {camera: true});
+    
+    var xhttp = new XMLHttpRequest();
+    var current_meet_id = window.location.href.split('/');
+  
+    tracker.on('track', function(event) {
+      event.data.forEach(function(rect) {
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+          }
+        };
+        xhttp.open("GET", BACKEND_URLS.facetracker + '?user=' + logged_in_user.email + '&meet_id=' + current_meet_id[current_meet_id.length - 1], true);
+        xhttp.send();
+      });
     });
-  });
-})();
+  }
+  main_function();
+  setInterval(sendData, 10000);
+}
+
